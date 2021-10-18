999f41e5c9031f769b46d0036a7263dc2ceaf7fe
Julian krochmal <jkrochmal.ext@andromedalatam.com>
2021-10-18 15:11:07 -0300

    updating user controller

--- routes/projects.js
+++ routes/projects.js
@@ -2,14 +2,18 @@ const express = require('express')
 const router = express.Router()
 const projectsController = require('../controllers/projectsController')
 const auth = require('../middleware/auth')
+const {check} = require('express-validator')
 
 router.post('/',
     auth,
+    [
+        check('name', 'Project name must be completed').not().isEmpty()
+    ],
     projectsController.createProject
 )
 
 router.get('/',
     auth,
-    projectsController.createProject
+    projectsController.getProjects
 )
 module.exports = router
